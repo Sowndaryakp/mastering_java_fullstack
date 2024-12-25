2094c43809971ac03999b27392472c7bbe6fd1e2
Sowndaryakp <sonalshetty.1999@gmail.com>
2024-12-25 22:25:09 +0530

    basic concepts

---
+++ Core Java/basics/Hello.java
@@ -0,0 +1,46 @@
+package basics;
+public class Hello{
+    public static void main(String args[])
+    {
+        // System.out.println("Hello");
+
+        // //Variables
+        // System.out.println(8+1);
+        // System.out.print(6+1);
+
+        // //Data Type - Primitiv
+        // byte b = 127; //127 is last value
+        // System.out.println(b);
+
+        // short b = 558;
+        // System.out.println(b);
+        
+        // int num1 = 5;
+        // int num2 = 1;
+        // int result = num1 + num2;
+        // System.out.println(result);
+
+        // long l = 12l;
+        // System.out.println(l);
+
+        // float f = 7.9f;
+        // System.out.println(f);
+
+        // double d = 78.9;
+        // System.out.println(d);
+
+        // char c ='s';
+        // System.out.println(c);
+
+        // boolean b = true;
+        // System.out.println(b);
+
+        // //literals
+        // int num1 = 0b101; //binary
+        // int num1 = 0x7E; //hexa
+        // System.out.println(num1);
+
+
+    }
+    
+}
