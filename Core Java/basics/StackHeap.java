9cd9af606d7cc332ab7752e509cae8e330c2564f
Sowndaryakp <sonalshetty.1999@gmail.com>
2025-01-03 23:22:44 +0530

    working on stack and heap memory

---
+++ Core Java/basics/StackHeap.java
@@ -0,0 +1,13 @@
+class Calculator2{
+    int num = 5;
+}
+public class StackHeap {
+    public static void main(String[] args){
+        Calculator2 calc1 = new Calculator2();
+        Calculator2 calc2 = new Calculator2();
+
+        System.out.println(calc1.num);
+        System.out.println(calc2.num);
+
+    }
+}
