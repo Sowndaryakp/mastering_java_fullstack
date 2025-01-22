bce355636b8256ca28f0b93029af8b442d292fdd
Sowndaryakp <sonalshetty.1999@gmail.com>
2025-01-22 22:02:04 +0530

    working on basic patterns

---
+++ Core Java/basics/programming/patterns.java
@@ -0,0 +1,31 @@
+package programming;
+
+public class patterns {
+    public static void main(String[] args){
+        //5*5 star
+        // for(int i=1;i<5;i++){
+        //     for(int j=1;j<5;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println("");
+        // }
+
+        //1 to 5 equal
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=5;j++){
+        //         System.out.print(i);
+        //     }
+        //     System.out.println("");
+        // }
+
+        //same above with diff values
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+
+    }
+    
+}
