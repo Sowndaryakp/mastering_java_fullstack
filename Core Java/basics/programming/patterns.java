7b2168abd3efa6b33e15a909b93113d8a960cedb
Sowndaryakp <sonalshetty.1999@gmail.com>
2025-01-24 23:26:40 +0530

    continue working on patterns

--- Core Java/basics/programming/patterns.java
+++ Core Java/basics/programming/patterns.java
@@ -2,7 +2,7 @@ package programming;
 
 public class patterns {
     public static void main(String[] args){
-        //5*5 star
+        //1. 5*5 star
         // for(int i=1;i<5;i++){
         //     for(int j=1;j<5;j++){
         //         System.out.print("*");
@@ -10,7 +10,7 @@ public class patterns {
         //     System.out.println("");
         // }
 
-        //1 to 5 equal
+        //2. 1 to 5 equal
         // for(int i=1;i<=5;i++){
         //     for(int j=1;j<=5;j++){
         //         System.out.print(i);
@@ -18,14 +18,32 @@ public class patterns {
         //     System.out.println("");
         // }
 
-        //same above with diff values
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<=5;j++){
-                System.out.print(j);
-            }
-            System.out.println("");
-        }
+        //3. same above with diff values
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=5;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println("");
+        // }
 
+        //4. 
+        // for(int i=5;i>=1;i--){
+        //     for(int j=5;j>=1;j--){
+        //         System.out.print(i);
+        //     }
+        //     System.out.println("");
+        // }
+
+        //5. reverse
+        // for(int i=5;i>=1;i--){
+        //     for(int j=5;j>=1;j--){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println("");
+        // }
+
+        //6. 
+        
     }
     
 }
