6cdb85749d8cca24c26777f8f937f3e7a06988bb
Sowndaryakp <sonalshetty.1999@gmail.com>
2025-02-05 23:10:06 +0530

    working on patterns

--- Core Java/basics/programming/patterns.java
+++ Core Java/basics/programming/patterns.java
@@ -42,7 +42,19 @@ public class patterns {
         //     System.out.println("");
         // }
 
-        //6. 
+        // //6. 1 to 25 all numbers
+        // int n = 5;
+        // int k = 1;
+        // for(int i=1; i<=n; i++){
+        //     for(int j = 1; j<=n;j++){
+        //         System.out.print(k);
+        //         k++;
+        //     }
+        //     System.out.println();
+        // }
+
+        // //7. odd numbers
+        
         
     }
     
