ddb2082ee45118c90c04342227ba6209736502cd
Sowndaryakp <sonalshetty.1999@gmail.com>
2025-03-05 16:48:43 +0530

    woring on patterns

---
+++ PatternsCoding/BasicPatterns.java
@@ -0,0 +1,171 @@
+class BasicPatterns{
+    public static void main(String[] args){
+
+        // //1
+        // *****
+        // *****
+        // *****
+        // *****
+        // *****
+
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=5;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // //2
+        // 11111
+        // 22222
+        // 33333
+        // 44444
+        // 55555
+
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=5;j++){
+        //         System.out.print(i);
+        //     }
+        //     System.out.println();
+        // }
+
+        // //3
+        // 12345
+        // 12345
+        // 12345
+        // 12345
+        // 12345
+
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=5;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        // //4
+        // 55555
+        // 44444
+        // 33333
+        // 22222
+        // 11111
+
+        // for(int i=5;i>=1;i--){
+        //     for(int j=5;j>=1;j--){
+        //         System.out.print(i);
+        //     }
+        //     System.out.println();
+        // }
+
+        // //5
+        // 54321
+        // 54321
+        // 54321
+        // 54321
+        // 54321
+
+        // for(int i=5;i>=1;i--){
+        //     for(int j=5;j>=1;j--){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        // //6
+        // 1 2 3 4 5
+        // 6 7 8 9 10
+        // 11 12 13 14 15
+        // 16 17 18 19 20
+        // 21 22 23 24 25
+
+        // int n = 5;
+        // int k = 1;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n;j++){
+        //         System.out.print(k);
+        //         k++;
+        //     }
+        //     System.out.println();
+        // }
+
+        // //7
+        // 1 3 5 7 9
+        // 11 13 15 17 19
+        // 21 23 25 27 29
+        // 31 33 35 37 39
+        // 41 43 45 47 49
+
+        // int n = 5;
+        // int k = 1;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n;j++){
+        //         System.out.print(k + " ");
+        //         k += 2;
+        //     }
+        //     System.out.println();
+        // }
+
+        // //8
+        // 2 4 6 8 10
+        // 12 14 16 18 20
+        // 22 24 26 28 30
+        // 32 34 36 38 40
+        // 42 44 46 48 50
+
+        // int n = 5;
+        // int k = 2;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n;j++){
+        //         System.out.print(k + " ");
+        //         k += 2;
+        //     }
+        //     System.out.println();
+        // }
+
+        // //9
+        // 1 2 3 4 5
+        // 2 4 6 8 10
+        // 3 6 9 12 15 
+        // 4 8 12 16 20
+        // 5 10 15 20 25
+
+        // int n = 5;
+        
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n;j++){
+        //         System.out.print(j * i + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // //10
+        // 1 1 2 1 3 1
+        // 1 2 2 2 3 2
+        // 1 3 2 3 3 3
+        // 1 2 2 4 3 4
+        // 1 5 2 5 3 5
+        
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=3;j++){
+        //         System.out.print(j + " " + i + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // //11
+        // 1 1 2 1 3 1
+        // 1 2 2 2 3 2
+        // 1 3 2 3 3 3
+        // 1 2 2 4 3 4
+        // 1 5 2 5 3 5
+        
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=3;j++){
+        //         System.out.print(i + " " + j + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+    }
+}
