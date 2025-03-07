75e0116518daeef5b7fc0f2e931a1a70a0f5e799
Sowndaryakp <sonalshetty.1999@gmail.com>
2025-03-07 17:05:55 +0530

    core java coding practices

--- Projects/OEP-MyIdea/backend/src/main/java/com/oep/service/EmailService.java
+++
@@ -1,6 +0,0 @@
-package com.oep.service;
-
-public interface EmailService {
-    void sendEmail(String to, String subject, String content);
-    void sendHtmlEmail(String to, String subject, String htmlContent);
-} 
